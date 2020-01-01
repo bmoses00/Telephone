115a8cbf709ec87d95fbc1066ff85f3bb6c5fc4f
Brian <bmoses00@stuy.edu>
2019-12-31 21:54:19 -0500

    Added semaphore... does it work? I think so?

--- control.c
+++ control.c
@@ -1,12 +1,25 @@
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/shm.h>
 # include <fcntl.h>
 # include <string.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include <sys/sem.h>
 
-# define KEY 42
+# define SHMKEY 42
+# define SEMKEY 500
 # define SIZE 100
+
+union semun {
+   int              val;    /* Value for SETVAL */
+   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+   unsigned short  *array;  /* Array for GETALL, SETALL */
+   struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                               (Linux-specific) */
+};
 
 void display_story() {
   int fd = open("story.txt", O_RDONLY);
@@ -22,15 +35,28 @@ void display_story() {
 void remove_content() {
   if (!fork())
     execlp("rm", "rm", "story.txt", NULL);
-  int shmd = shmget(KEY, SIZE, IPC_CREAT | 0600);
+  int w;
+  wait(&w);
+  int shmd = shmget(SHMKEY, SIZE, IPC_CREAT | 0600);
   shmctl(shmd, IPC_RMID, 0);
-  printf("Removed file\nRemoved shared memory segment\n");
+
+  struct sembuf semaphore;
+  semaphore.sem_num = 0;
+  semaphore.sem_op = -1;
+  int semd = semget(SEMKEY, 1, 0600);
+  semop(semd, &semaphore, 1);
+  semctl(semd, IPC_RMID, 0);
+  printf("Removed file\nRemoved shared memory segment\nRemoved semaphore\n");
 }
 
 void create_content() {
-  int shmd = shmget(KEY, SIZE, IPC_CREAT | 0600);
+  shmget(SHMKEY, SIZE, IPC_CREAT | 0600);
   open("story.txt", O_CREAT|O_WRONLY|O_TRUNC, 0777);
-  printf("File created\nShared memory segment created\n");
+  int semd = semget(SEMKEY, 1, 0600);
+  union semun us;
+  us.val = 1;
+  semctl(semd, 0, SETVAL, us);
+  printf("File created\nShared memory segment created\nSemaphore created\n");
 }
 
 int main(int argc, char * argv[]) {
--- write.c
+++ write.c
@@ -5,13 +5,30 @@
 # include <string.h>
 # include <sys/ipc.h>
 # include <sys/shm.h>
+# include <sys/sem.h>
 
-# define KEY 42
+# define SHMKEY 42
+# define SEMKEY 500
 # define SIZE 100
 
+union semun {
+   int              val;    /* Value for SETVAL */
+   struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+   unsigned short  *array;  /* Array for GETALL, SETALL */
+   struct seminfo  *__buf;  /* Buffer for IPC_INFO
+                               (Linux-specific) */
+};
+
 int main() {
-  int shmd = shmget(KEY, SIZE, 0);
+  int shmd = shmget(SHMKEY, SIZE, 0);
   int * length = shmat(shmd, 0, 0);
+
+  int semd = semget(SEMKEY, 1, 0600);
+  struct sembuf semaphore;
+  semaphore.sem_num = 0;
+  semaphore.sem_flg = SEM_UNDO;
+  semaphore.sem_op = -1;
+  semop(semd, &semaphore, 1);
 
   int fd = open("story.txt", O_RDWR);
 
@@ -22,8 +39,6 @@ int main() {
   for (i = 0; i < 100; i++) {
     buffer[i] = 0;
   }
-  // could potenetially replace above line with nulling out byte after (length)
-  // also: use semaphore!
   read(fd, buffer, *length);
   printf("%s\n", buffer);
   printf("Your addition: \n");
